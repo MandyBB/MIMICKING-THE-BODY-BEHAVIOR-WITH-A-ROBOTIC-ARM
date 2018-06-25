39e47d1f80bc15932573e4a149ee3d44be3924d4
MandyBB <39490473+MandyBB@users.noreply.github.com>
2018-06-25 16:10:34 +0200

    Add files via upload

---
+++ Robotic_Arm.ino
@@ -0,0 +1,23 @@
+#include "Libraries.h"
+
+void setup() {
+      //Servo motor
+      SetupServoArm(); 
+      clock_begin = millis();
+      //Wifi configuration
+      UDPSetup();
+}
+
+void loop() {
+  //loopServoDance();
+  clock_end = millis();   
+ 
+  if (clock_end > clock_begin + 1,77) {
+    clock_begin = clock_end;
+
+    UDP_Reception();
+    MoveMyArm();
+  }
+     
+}
+
